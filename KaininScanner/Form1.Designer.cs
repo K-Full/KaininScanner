163eca6c9d26006fd2394eb8dc89f3f7a17f0ca8
K-Full <e186jp@gmail.com>
2019-09-24 21:29:50 +0900

    create

--- KaininScanner/Form1.Designer.cs
+++ KaininScanner/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 9F);
+            this.button1.Location = new System.Drawing.Point(0, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 154);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Snip";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(264, 153);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "KaininScanner";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- KaininScanner/Form1.cs
+++ KaininScanner/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KaininScanner
@@ -15,6 +9,46 @@ namespace KaininScanner
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static Image Snip()
+        {
+            var rc = Screen.PrimaryScreen.Bounds;
+            using (Bitmap bmp = new Bitmap(rc.Width, rc.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb))
+            {
+                using (Graphics gr = Graphics.FromImage(bmp))
+                    gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                using (var snipper = new DummyForm(bmp))
+                {
+                    if (snipper.ShowDialog() == DialogResult.OK)
+                    {
+                        return snipper.Image;
+                    }
+                }
+                return null;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (var bmp = Snip())
+            {
+                if (bmp != null)
+                {
+                    using (var image = new Bitmap(bmp))
+                    {
+                        using (var ocr = new Tesseract.TesseractEngine(@"./tessdata", "eng", Tesseract.EngineMode.Default))
+                        {
+                            ocr.SetVariable("tessedit_char_whitelist", "0123456789");
+                            using (var result = ocr.Process(image, Tesseract.Rect.Empty))
+                            {
+                                Clipboard.SetText(result.GetText().Trim());
+                            }
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
